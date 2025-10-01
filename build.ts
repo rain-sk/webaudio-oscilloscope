e37c7929deb80e6218bfe3155961dc7dbf9ec14d
Rain <rain@synth.kitchen>
2025-10-01 21:11:39 +0200

    rough port to typescript

---
+++ build.ts
@@ -0,0 +1,21 @@
+import esbuild from "esbuild";
+
+esbuild
+  .build({
+    entryPoints: ["./src/index.ts"],
+    outdir: "./build",
+    external: ["server-name-gen", "uuid"],
+    bundle: true,
+    minify: true,
+    keepNames: true,
+    sourcemap: false,
+    platform: "browser",
+    target: "esnext",
+    format: "esm",
+    color: true,
+    tsconfig: "./tsconfig.json",
+  })
+  .then(() => console.log("⚡ Bundle build complete ⚡"))
+  .catch(() => {
+    process.exit(1);
+  });
---
+++ src/index.ts
@@ -0,0 +1,140 @@
+import {
+  AudioContext,
+  IAnalyserNode,
+  IAudioDestinationNode,
+  IAudioNode,
+} from "standardized-audio-context";
+
+import { initCvs, primer as defaultPrimer, drawRawOsc } from "./canvas-tools";
+
+type CanvasHookFunction = (
+  context: CanvasRenderingContext2D,
+  width: number,
+  height: number
+) => void;
+
+export class Oscilloscope {
+  private paused = false;
+  private analyser: IAnalyserNode<AudioContext>;
+  private width = 300;
+  private height = 150;
+
+  private init: CanvasHookFunction;
+  private primer: CanvasHookFunction;
+
+  private u8ar: Uint8Array;
+
+  private cctx: CanvasRenderingContext2D;
+  constructor(
+    private actx: AudioContext,
+    private src: IAudioNode<AudioContext>,
+    private cvs: HTMLCanvasElement,
+    private dest?: IAudioDestinationNode<AudioContext>,
+    private FFT: number = 2048,
+    init?: CanvasHookFunction,
+    primer?: CanvasHookFunction
+  ) {
+    this.init = init ?? initCvs;
+    this.primer = primer ?? defaultPrimer;
+    this.analyser = this.actx.createAnalyser();
+    // Configure Analyzer
+    this.analyser.fftSize = this.FFT;
+    this.src.connect(this.analyser);
+    if (this.dest) this.analyser.connect(this.dest);
+    // Set up Canvas
+    let { width = 300, height = 150 } = this.cvs;
+    this.width = width;
+    this.height = height;
+    this.u8ar = new Uint8Array(this.FFT);
+    const cctx = this.cvs.getContext("2d");
+    if (!cctx) {
+      throw new Error("failed to initialize Oscilloscope <canvas> element");
+    }
+    this.cctx = cctx;
+    this.init(this.cctx, this.width, this.height);
+  }
+
+  draw = () => {
+    if (!this.paused) {
+      requestAnimationFrame(this.draw);
+    }
+    this.cctx.clearRect(0, 0, this.width, this.height);
+    this.primer(this.cctx, this.width, this.height);
+    this.analyser.getByteTimeDomainData(this.u8ar);
+    drawRawOsc(this.cctx, this.u8ar, this.width, this.height);
+  };
+
+  start = () => {
+    this.paused = false;
+    this.draw();
+  };
+
+  pause = () => {
+    this.paused = true;
+  };
+
+  reset = () => {
+    this.paused = true;
+    requestAnimationFrame(() => {
+      this.u8ar = new Uint8Array(this.FFT).fill(0);
+      this.cctx.clearRect(0, 0, this.width, this.height);
+      this.primer(this.cctx, this.width, this.height);
+      drawRawOsc(this.cctx, this.u8ar, this.width, this.height);
+    });
+  };
+}
+
+export function createAudioContext() {
+  return new AudioContext();
+}
+
+export function MediaStreamOscilloscope(
+  mediaStream: MediaStream,
+  canvasElement: HTMLCanvasElement,
+  audioDest?: IAudioDestinationNode<AudioContext>,
+  analyzerFFT = 2048,
+  canvasInitFunction?: CanvasHookFunction,
+  drawingPrimerFunction?: CanvasHookFunction
+) {
+  let ctx = createAudioContext();
+  let src = ctx.createMediaStreamSource(mediaStream);
+  return new Oscilloscope(
+    ctx,
+    src,
+    canvasElement,
+    audioDest,
+    analyzerFFT,
+    canvasInitFunction,
+    drawingPrimerFunction
+  );
+}
+
+export function getUserMedia(constraints: MediaStreamConstraints) {
+  return new Promise((resolve) => {
+    if (
+      navigator &&
+      navigator.mediaDevices &&
+      navigator.mediaDevices.getUserMedia
+    ) {
+      navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then(resolve)
+        .catch(() => resolve(null));
+    } else if (
+      "getUserMedia" in navigator &&
+      typeof navigator.getUserMedia === "function"
+    ) {
+      navigator.getUserMedia(constraints, resolve, () => resolve(null));
+    } else if (
+      "webkitGetUserMedia" in navigator &&
+      typeof navigator.webkitGetUserMedia === "function"
+    ) {
+      navigator.webkitGetUserMedia(constraints, resolve, () => resolve(null));
+    } else if (
+      "mozGetUserMedia" in navigator &&
+      typeof navigator.mozGetUserMedia === "function"
+    ) {
+      navigator.mozGetUserMedia(constraints, resolve, () => resolve(null));
+    } else resolve(null);
+  });
+}
